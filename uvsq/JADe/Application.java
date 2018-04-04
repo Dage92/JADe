49644da5f541bfa81b6e53d6ec376bf450087de5
Anis Bouziane <lewebpropulsion@gmail.com>
2018-04-04 16:41:55 +0200

    hello there

--- uvsq/JADe/Application.java
+++ uvsq/JADe/Application.java
@@ -3,7 +3,8 @@ import java.lang.*;
 
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("Hello there!");
     }
 }
