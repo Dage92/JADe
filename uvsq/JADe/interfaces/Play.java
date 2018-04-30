2d460b672d5a554a4bba46d9be5f14109dcabedc
Dage92 <david.goodman92@gmail.com>
2018-04-30 23:23:41 +0200

    Ajout de certains cas d'inputs et de la visualisation du monde.

--- uvsq/JADe/interfaces/Play.java
+++ uvsq/JADe/interfaces/Play.java
@@ -2,6 +2,7 @@ package interfaces;
 
 import asciiPanel.AsciiPanel;
 import world.World;
+import world.WorldGenerator;
 
 import java.awt.event.KeyEvent;
 import java.util.ArrayList;
@@ -17,6 +18,7 @@ public class Play implements Window{
         height = 80;
         width = 23;
         msg = new ArrayList<String>();
+        createWorld();
     }
 
     private void createCreature(){
@@ -27,33 +29,77 @@ public class Play implements Window{
 
     }
 
+    private void createWorld(){
+        world = new WorldGenerator().makeDungeon();
+    }
+
     public int scrollX(){
-    return 0;
+        return Math.max(0, Math.min(player.x - width / 2, world.width() - width));
     }
 
     public int scrollY(){
-        return 0;
+        return Math.max(0, Math.min(player.y - height / 2, world.height() - height));
     }
 
     public void reaction(){
 
     }
 
-    public void displayPanes(){
+    public void displayPanes(AsciiPanel panel, int left, int top){
+        for (int x = 0; x < width; x++){
+            for (int y = 0; y < height; y++){
+                int wx = x + left;
+                int wy = y + top;
 
+                panel.write(world.glyph(wx, wy, player.z), x, y, world.color(wx, wy, player.z));
+            }
+        }
     }
 
-    public void displayMsg(){
+    public void displayMsg(AsciiPanel panel, List<String> msg){
 
     }
 
     @Override
     public void display(AsciiPanel panel) {
+        int left = scrollX();
+        int top = scrollY();
 
+        displayPanes(panel, left, top);
     }
 
     @Override
     public Window reaction(KeyEvent key) {
-        return null;
+        switch (key.getKeyCode()) {
+            case KeyEvent.VK_LEFT:
+            case KeyEvent.VK_H:
+                player.moveBy(-1, 0, 0);
+                break;
+            case KeyEvent.VK_RIGHT:
+            case KeyEvent.VK_L:
+                player.moveBy(1, 0, 0);
+                break;
+            case KeyEvent.VK_UP:
+            case KeyEvent.VK_K:
+                player.moveBy(0, -1, 0);
+                break;
+            case KeyEvent.VK_DOWN:
+            case KeyEvent.VK_J:
+                player.moveBy(0, 1, 0);
+                break;
+            case KeyEvent.VK_Y:
+                player.moveBy(-1, -1, 0);
+                break;
+            case KeyEvent.VK_U:
+                player.moveBy(1, -1, 0);
+                break;
+            case KeyEvent.VK_B:
+                player.moveBy(-1, 1, 0);
+                break;
+            case KeyEvent.VK_N:
+                player.moveBy(1, 1, 0);
+                break;
+        }
+        return this;
     }
 }
