7fd0763f5f5c192cbc323c45f469b005806a7d8a
SolowinFXVI <julien.n.jacquet@gmail.com>
2018-05-08 11:41:57 +0200

    added WriteObject(Object) to save world

--- uvsq/JADe/Ecriture.java
+++ uvsq/JADe/Ecriture.java
@@ -1,5 +1,29 @@
-public class Ecriture {
+import java.io.*;
 
+/**
+ * La Classe {@code Ecriture} sert a sauvegarder l'état d'un monde avant de quitter
+ *@author JACQUET Julien 21400579
+ */
 
+public class Ecriture implements Serializable {
 
+    private static final String filepath =" ../save/save1.ser";
+
+    /**
+     * Le methode ecrit l'objet {@code World} dans un fichier.
+     * @param serObj
+     * @throws IOException
+     */
+    public void WriteObject(Object serObj) throws IOException {
+        ObjectOutputStream objOut = null;
+        try{
+            FileOutputStream fout = new FileOutputStream(filepath);
+            objOut = new ObjectOutputStream(fout);
+            objOut.writeObject(serObj);
+        }catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            objOut.close();
+        }
+    }
 }
