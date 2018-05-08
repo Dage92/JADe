1741e4fb880ae6e96d68bc1532b97440f9f1aa57
SolowinFXVI <julien.n.jacquet@gmail.com>
2018-05-08 11:42:40 +0200

    added load() to load world for savefile

--- uvsq/JADe/Lecture.java
+++ uvsq/JADe/Lecture.java
@@ -1,7 +1,32 @@
+import world.World;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+/**
+ * La classe {@code Lecture} sert a charger le monde avec un fichier de sauvegarde.
+ * @author JACQUET Julien 21400579
+ */
+
 public class Lecture{
-    public Lecture(){
 
+    /**
+     * La methode Load charge un nouveau {@code world} depuis un fichier de sauvegarde situe a : "../save/save1.ser"
+     * @throws IOException
+     */
+    public void load() throws IOException {
+        ObjectInputStream serObj = null;
+        try {
+            FileInputStream fin = new FileInputStream("../save/save1.ser");
+            serObj = new ObjectInputStream(fin);
+            World worldread = (World) serObj.readObject();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (serObj != null) {
+                serObj.close();
+            }
+        }
     }
-
-
 }
