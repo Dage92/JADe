651b447a74584a95829bb5449076646583c07b21
SolowinFXVI <julien.n.jacquet@gmail.com>
2018-05-08 11:39:51 +0200

    updated javadoc and added color

--- uvsq/JADe/world/Pane.java
+++ uvsq/JADe/world/Pane.java
@@ -1,33 +1,55 @@
 package world;
 
-public enum Pane{
-  SOL((char)176, "Le sol."),
-  MUR((char) 219,"Un mur infranchissable."),
-  LIMITE((char) 255, "Nul ne sait ce qui ce passe par dela cette limite");
+import asciiPanel.AsciiPanel;
 
+import java.awt.*;
+
+/**
+ * L'enumeration des {@code Pane} a l'origine on souhaitait appeler cette classe {@code Case} pour des raisons evidentes mais "case" (en minuscule) existe deja en JAVA et entrainait toutes sortes de conflits.
+ * @author JACQUET Julien 21400579
+ */
+public enum Pane{
+  SOL((char)176, AsciiPanel.brightBlack ,"Le sol."),
+  MUR((char) 219,AsciiPanel.white, "Un mur infranchissable."),
+  LIMITE((char) 255, AsciiPanel.brightWhite, "Nul ne sait ce qui ce passe par dela cette limite");
   private char symbole;
   private String def;
+  private Color color;
 
+    /**
+     * get de {@code symbole}
+     * @return char
+     */
   public char symbole(){
-
       return symbole;
     }
 
-
-  public String getDef(){
-
+    /**
+     * get de {@code def}
+     * @return String
+     */
+  public String def(){
       return def;
     }
 
+    /**
+     * get de {@code color}
+     * @return
+     */
+    public Color color(){
+      return color;
+    }
 
 
-  Pane(char symbole, String def) {
+    /**
+     * constructeur d'une Case/Pane.
+     * @param symbole
+     * @param brightBlack
+     * @param def
+     */
+  Pane(char symbole, Color brightBlack, String def) {
         this.symbole = symbole;
+        this.color = color;
         this.def = def;
   }
-
-
-
-
-
 }
